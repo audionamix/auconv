2ce2a3384cd4ddcd0358b28f975c54c058464b32
guillaume_vincke <guillaume.vincke@audionamix.com>
2017-11-09 17:20:39 +0100

    Update ffmpeg. Set osx target to 10.11. Add failing tests

--- src/auconv/file_test.cc
+++ src/auconv/file_test.cc
@@ -13,14 +13,59 @@ TEST(File, Convert) {
   ASSERT_EQ(file.sample_rate(), 48000);
   ASSERT_EQ(file.channel_count(), 2);
   ASSERT_EQ(file.bits_per_samples(), 16);
-  
+
   file.Export("/tmp/file.wav", auconv::Format::kWav, err);
   ASSERT_FALSE(err);
-  
+
   file.set_sample_rate(44100);
   file.set_bits_per_samples(24);
   file.set_mono(true);
-  
+
   file.Export("/tmp/file_mono.wav", auconv::Format::kWav, err);
   ASSERT_FALSE(err);
 }
+
+TEST(File, ConvertErrorM4a) {
+  std::string path = gResourcePath + "/EverythingWasEverything.m4a";
+  std::error_code err;
+
+  auconv::File file;
+  file.Open(path, err);
+  ASSERT_FALSE(err);
+  ASSERT_EQ(file.sample_rate(), 44100);
+  ASSERT_EQ(file.channel_count(), 2);
+
+  file.Export("/tmp/file-m4a.wav", auconv::Format::kWav, err);
+  ASSERT_FALSE(err);
+}
+
+TEST(File, ConvertErrorMP3) {
+  std::string path = gResourcePath + "/MajorLazer-LeanOn.mp3";
+  std::error_code err;
+
+  auconv::File file;
+  file.Open(path, err);
+  ASSERT_FALSE(err);
+  ASSERT_EQ(file.sample_rate(), 44100);
+  ASSERT_EQ(file.channel_count(), 2);
+  //  ASSERT_EQ(file.bits_per_samples(), 16);
+
+  file.Export("/tmp/file-mp3.wav", auconv::Format::kWav, err);
+  ASSERT_FALSE(err);
+}
+
+TEST(File, ConvertErrorAIFF) {
+  std::string path = gResourcePath + "/The_Chainsmokers-Something_Just_Like_Thi-LLS.aif";
+  std::error_code err;
+
+  auconv::File file;
+  file.Open(path, err);
+  ASSERT_FALSE(err);
+  ASSERT_EQ(file.sample_rate(), 44100);
+  ASSERT_EQ(file.channel_count(), 2);
+  //  ASSERT_EQ(file.bits_per_samples(), 16);
+
+  file.Export("/tmp/file-aif.wav", auconv::Format::kWav, err);
+  ASSERT_FALSE(err);
+}
+
